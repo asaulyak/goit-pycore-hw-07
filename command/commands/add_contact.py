d1fae2502d561a7c177b9fa854cfe3175caac595
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2025-03-04 20:54:13 +0200

    feat: support adding multiple phone numbers

--- command/commands/add_contact.py
+++ command/commands/add_contact.py
@@ -31,7 +31,7 @@ def run(args, context: AddressBook):
     if not phone.isdigit():
         raise ValueError("Invalid phone number.")
 
-    contact = Record(name)
+    contact = context.get(name, Record(name))
     contact.add_phone(phone)
 
     message = "Contact added."
