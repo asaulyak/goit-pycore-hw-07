4ff93975cc160e5a6600b9e2e724e53492770cda
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2025-03-04 20:49:16 +0200

    feat: add __str__ method for address book

--- address_book/address_book.py
+++ address_book/address_book.py
@@ -5,6 +5,7 @@ class AddressBook(UserDict):
     def add_record(self, record):
         self.data[record.name.value] = record
 
+
     def find(self, name):
         for record in self.data.values():
             if record.name.value == name:
@@ -12,5 +13,10 @@ class AddressBook(UserDict):
 
         return None
 
+
     def delete(self, name):
         del self.data[name]
+
+
+    def __str__(self):
+        return f'Address Book:\n {'\n '.join(str(record) for record in self.data.values())}'
