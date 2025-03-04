236ecb1155907860486820cdcfe04a3f2cf14c0a
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2025-03-04 20:49:46 +0200

    feat: fix show_all command

--- command/commands/show_all.py
+++ command/commands/show_all.py
@@ -3,7 +3,6 @@ COMMAND_NAME = 'all'
 def run(_, context):
 
     message = str(context)
-    updated_context = context
     stop = False
 
-    return message, updated_context, stop
+    return message, stop
